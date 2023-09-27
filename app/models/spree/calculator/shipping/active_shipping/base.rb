1b8214cb13f994df13bb05a3ec4eea5bcdd5cee0
umairrazam <omairrr.azam@gmail.com>
2023-09-27 19:04:00 +0500

    fix other ups services

--- app/models/spree/calculator/shipping/active_shipping/base.rb
+++ app/models/spree/calculator/shipping/active_shipping/base.rb
@@ -104,7 +104,7 @@ module Spree
                 [CGI.unescapeHTML(service_name), rate.price]
               end  
             end
-            rate_hash = Hash[*rates.flatten]
+            rate_hash = Hash[*rates.compact.flatten]
             return rate_hash
           rescue ::ActiveShipping::Error => e
 
@@ -270,14 +270,14 @@ module Spree
         end
 
         def retrieve_rates_from_cache package, origin, destination
-          Rails.cache.fetch(cache_key(package)) do
+          # Rails.cache.fetch(cache_key(package)) do
             shipment_packages = packages(package)
             if shipment_packages.empty?
               {}
             else
               retrieve_rates(origin, destination, shipment_packages)
             end
-          end
+          # end
         end
       end
     end
